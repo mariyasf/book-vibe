f684091a1af79d484a20625ed501d7a006b9c050
mariya-sultana <mariasultanafahmida@gmail.com>
2024-03-27 11:38:25 +0600

    linked banner button with listedpage

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,11 +1,15 @@
 import { Button } from "@material-tailwind/react";
+import { Link } from "react-router-dom";
 
 const Banner = () => {
     return (
-        <div className="space-y-6 p-20 lg:flex gap-10 justify-center text-center lg:text-left items-center mt-10 mx-10 rounded-xl  lg:h-[450px] bg-blue-gray-100">
+        <div className="space-y-6 p-10 lg:p-20 lg:flex gap-10 justify-center text-center lg:text-left items-center mt-10 mx-10 rounded-xl  lg:h-[450px] bg-blue-gray-100">
             <div className="space-y-6">
-                <h1 className="text-4xl font-semibold">Books to freshen up your bookshelf</h1>
-                <Button size="lg" className="bg-green-600">View The List</Button>
+                <h1 className="text-4xl font-semibold mb-4">Books to freshen up your bookshelf</h1>
+                <Link to={"/listedBooks"}>
+                    <Button size="lg" className="bg-[#23BE0A]">View The List</Button>
+
+                </Link>
             </div>
             <div >
                 <img className="h-[400px] rounded-2xl" src="images/banner.png" alt="" />
--- src/Components/BookList/BookList.jsx
+++ src/Components/BookList/BookList.jsx
@@ -5,7 +5,7 @@ import CustomSpinner from "../CustomSpinner/CustomSpinner";
 const BookList = () => {
     const { data, loading } = useBookListData();
     // console.log("Home Data: ", data);
-    
+
     if (loading) {
         return <CustomSpinner />
     }
@@ -13,7 +13,11 @@ const BookList = () => {
 
     return (
         <div>
-            <div className="mt-5 grid grid-cols-2 lg:grid-cols-3 gap-5 mx-10">
+            <div className="flex justify-center my-10">
+                <h2 className="text-3xl font-bold">Books</h2>
+            </div>
+
+            <div className="mt-5 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mx-10">
                 {
                     data.map(book =>
                         <BookCard key={book.bookId} book={book} />
